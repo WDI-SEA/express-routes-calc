6f6c4138d9c7a6a439bc5e5027d2e3ac08a5c5a0
jtw007 <joshwu095@gmail.com>
2022-12-12 20:53:49 -0800

    bonus

--- index.js
+++ index.js
@@ -30,5 +30,35 @@ app.get("/divide/:x/:y", (req, res) => {
     res.send("The answer is: " + (parseInt(req.params.x) / parseInt(req.params.y)));
   });
 
+app.get("/*", (req, res) => {
+    //console.log(req.params)
+    let numbers = req.params[0].split('/')
+    //req.query.math => request.query(whatever comes after '/').math(mathematical functions)
+    if (req.query.math === 'add') {
+        let num = 0
+        for(let i = 0; i < numbers.length; i++) {
+            num += Number(numbers[i])
+        }
+        res.send(`The sum is ${num}`)
+    } else if (req.query.math === 'subtract') {
+        let num = 0
+        numbers.forEach(number => {
+            num -= Number(number)
+        })
+        res.send(`The difference is ${num}`)
+    } else if (req.query.math === 'multiply') {
+        let num = 1
+        numbers.forEach(number => {
+            num *= Number(number)
+        })
+        res.send(`The product is ${num}`)
+    } else if (req.query.math === 'divide') {
+        let num = 1
+        numbers.forEach(number => {
+            num /= Number(number)
+        })
+        res.send(`The quotient is ${num}`)
+    } 
+})
 
 app.listen(8000)
