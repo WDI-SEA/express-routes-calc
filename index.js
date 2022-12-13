beff70f77ab8776f9e73e890ad9485b741d0a23b
fmher <feumatthewher2@gmail.com>
2022-12-13 09:23:52 -0800

    fin

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const express = require("express")
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('Hello, Caluculator is me!')
+})
+
 app.get('/add/:num1/:num2', (req, res) => {
-    res.send(`${req.params.num1} + ${req.params.num2} =`, req.params.num1 + req.params.num2)
+    res.send(`${req.params.num1} + ${req.params.num2} =`, Number(req.params.num1) + Number(req.params.num2))
 })
 
 app.get('/subtract/:num1/:num2', (req, res) => {
-    res.send(`${req.params.num1} - ${req.params.num2} =`, req.params.num1 - req.params.num2)
+    res.send(`${req.params.num1} - ${req.params.num2} =`, Number(req.params.num1) - Number(req.params.num2))
 })
 
 app.get('/multiply/:num1/:num2', (req, res) => {
-    res.send(`${req.params.num1} * ${req.params.num2} =`, req.params.num1 * req.params.num2)
+    res.send(`${req.params.num1} * ${req.params.num2} =`, Number(req.params.num1) * Number(req.params.num2))
 })
 
 app.get('/divide/:num1/:num2', (req, res) => {
-    res.send(`${req.params.num1} / ${req.params.num2} =`, req.params.num1 / req.params.num2)
+    res.send(`${req.params.num1} / ${req.params.num2} =`, Number(req.params.num1) / Number(req.params.num2))
 })
 
 
 app.listen(8000, (req, res)=>{
-    console.log("listening to port " + 80000)
+    console.log("listening to port " + 8000)
     // let n = 1
     // let n2 = 2
     // console.log(`${n} + ${n2} =`, n*n2)
