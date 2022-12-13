322803d1e7741263811c4d7ccca95c1ceed9fff7
Jaywomack <womackjw@hotmail.com>
2022-12-12 16:22:18 -0800

    getting errors in console, but add function is working and returning the proper result

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const express = require('express')
 const app = express()
+let bodyParser = require('body-parser')
+
 const Calc = require('./Calc.js')
 // Calculator class with + - * / accepts any number of arguments
 let c = new Calc()
+// MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
+// ROUTES
 app.get('/*', (req, res) => {
 	const arithmetic = req.params[0]
-	console.log(arithmetic)
-	const args = []
+	let args = Object.keys(req.query)
+	console.log(args.join(','))
+	let nums = []
+	args[0].split(',').forEach((i) => nums.push(Number(i)))
+	console.log(nums)
+
+	// console.log(args)
 	switch (arithmetic) {
 		case 'add':
-			res.send(`${arithmetic}`)
-			// res.send(`${c.add(...args)}    `)
+			// res.send(`${arithmetic}`)
+
+			let result = c.add(...nums)
+			res.send(`${result}`)
 			break
 		case 'subtract':
 			res.send(`${arithmetic}`)
