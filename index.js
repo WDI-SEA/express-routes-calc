3f885b807fc8bfbcd4188e909beebf3a80e6a853
Andrew <andrew@andrewsmith.org>
2023-03-28 22:25:59 -0400

    Complete bonus

--- index.js
+++ index.js
@@ -21,6 +21,33 @@ app.get("/divide/:x/:y", (req, res) => {
   res.send("your total: " + parseInt(req.params.x) / parseInt(req.params.y));
 });
 
+app.get("/*", (req, res) => {
+  let parameters = req.params[0].split("/");
+  if (req.query.math === "add") {
+    let answer = parameters.reduce((total, num) => {
+      return total + parseInt(num);
+    }, 0);
+    res.send("Your answer: " + answer);
+  } else if (req.query.math === "subtract") {
+    let answer = parameters.reduce((total, num) => {
+      return total - parseInt(num);
+    });
+    res.send("Your answer: " + answer);
+  } else if (req.query.math === "multiply") {
+    let answer = parameters.reduce((total, num) => {
+      return total * parseInt(num);
+    });
+    res.send("Your answer: " + answer);
+  } else if (req.query.math === "divide") {
+    let answer = parameters.reduce((total, num) => {
+      return total / parseInt(num);
+    });
+    res.send("Your answer: " + answer);
+  } else {
+    res.send("that ain't math I know how to do, bucko");
+  }
+});
+
 app.listen(8000, () => {
   console.log("hiya");
 });
