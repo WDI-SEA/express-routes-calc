f0f4407dd844fa272e71328dc45cb4f7a92dcbc3
Heiner000 <garrett.heiner@gmail.com>
2023-03-29 09:41:24 -0700

    reviewed in class

--- index.js
+++ index.js
@@ -9,28 +9,73 @@ app.get("/", (req, res) => {
 
 //  add route
 app.get("/add/:x/:y", (req, res) => {
-    let num1 = parseInt(req.params.x)
-    let num2 = parseInt(req.params.y)
-    let sum = num1 + num2
+    let x = parseInt(req.params.x)
+    let y = parseInt(req.params.y)
+    let sum = x + y
     res.send(`The sum of ${req.params.x} and ${req.params.y} is ${sum}`)
 })
 
 //  subtract route
 app.get("/subtract/:x/:y", (req, res) => {
-    res.send(`The difference of ${req.params.x} and ${req.params.y} is ${req.params.x - req.params.y}`)
+    let x = parseInt(req.params.x)
+    let y = parseInt(req.params.y)
+    let difference = x - y
+    res.send(`The difference of ${req.params.x} and ${req.params.y} is ${difference}`)
 })
 
 //  multiply route
 app.get("/multiply/:x/:y", (req, res) => {
-    res.send(`The product of ${req.params.x} and ${req.params.y} is ${req.params.x * req.params.y}`)
+    let x = parseInt(req.params.x)
+    let y = parseInt(req.params.y)
+    let product = x - y
+    res.send(`The product of ${req.params.x} and ${req.params.y} is ${product}`)
 })
 
 //  divide route
 app.get("/divide/:x/:y", (req, res) => {
-    res.send(`The quotient of ${req.params.x} and ${req.params.y} is ${req.params.x / req.params.y}`)
+    let x = parseInt(req.params.x)
+    let y = parseInt(req.params.y)
+    let quotient = parseInt(x) / parseInt(y)
+    res.send(`The quotient of ${req.params.x} and ${req.params.y} is ${quotient}`)
 })
 
+// bonus
 
+app.get("/*", (req, res) => {
+    
+    let numsArray = req.params["0"].split("/")
+    console.log(numsArray)
+
+    if (req.query.math === "add") {
+        let result = 0
+        for (num of numsArray) {
+            result += parseInt(num)
+        }
+        res.send(`The result is ${result}`)
+    } else if (req.query.math === "subtract") {
+        let result = numsArray[0]
+        for (num of numsArray) {
+            result -= num
+        }
+        result += parseInt(numsArray[0])
+        res.send(`The result is ${result}`)
+    } else if (req.query.math === "multiply") {
+        let result = 1
+        for (num of numsArray) {
+            result *= parseInt(num)
+        }
+        res.send(`The result is ${result}`)
+    } else if (req.query.math === "divide") {
+        let result = numsArray[0]
+        for (num of numsArray) {
+            result /= parseInt(num)
+        }
+        result *= numsArray[0]
+        res.send(`The result is ${result}`)
+    } else {
+        res.send(req.query)
+    }
+})
 
 //  listen on port for requests
 app.listen(8000, () => [
