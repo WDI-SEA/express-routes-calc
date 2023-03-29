bed2e0130a31932cce14e89daa4ba9864c6806fc
username <youremail@domain.com>
2023-03-29 09:39:10 -0700

    added in the bonus that was done with the class

--- index.js
+++ index.js
@@ -24,6 +24,47 @@ app.get('/divide/:x/:y', (req, res) => {
 })
 
 
+//BONUS
+app.get('/*', (req, res)=>{
+    res.send(req.params)
+    console.log(req.query)
+
+    let numsArray = req.params["0"].split("/")
+    console.log(numsArray)
+
+    if(req.query.math === "add"){
+        let result = 0
+        for(num of numsArray){
+            result += parseInt(num)
+        }
+        res.send(`The result is ${result}`)
+    } else if(req.query.math === "subtract"){
+        let result = numsArray[0]
+
+        for(num of numsArray){
+            result -= parseInt(num)
+        }
+        result += parseInt(numsArray[0])
+        res.send(`The result is ${result}`)
+    } else if(req.query.math === "multiply"){
+        let result = 1
+        
+        for(num of numsArray){
+            result *= parseInt(num)
+        }
+    res.send(`The result is ${result}`)
+    } else if(req.query.math ==="divide"){
+        let result = numsArray[0]
+
+        for(num of numsArray){
+            results /= parseInt(num)
+        }
+            result *= numsArray[0]
+            res.send(`The result is ${result}`)
+    } else{
+        res.send(req.query)
+    }
+})
 
 app.listen(PORT, () =>{
     console.log(`listening on port ${PORT}`)
